4a6efa01f95c7cd0d90ee07be7cdbabaa421709b
DigitalCyan <marinoljubetic8@gmail.com>
2020-12-09 17:48:18 +0000

    Redone the generation help data in Help.ts

--- src/Commands/Help.ts
+++ src/Commands/Help.ts
@@ -6,12 +6,12 @@ import CommandModule from '../Interfaces/CommandModule';
 const module: CommandModule = {
     command: 'help',
     description: 'A help command!',
-    usage: `${BotMaster.instance.config.prefix}help`,
+    usage: `help`,
     function: (msg: Message, cmd: Command) => {
         let message = '```\nHELP\n---------------\n';
         BotMaster.instance.commandMaster.commandsMap.forEach((value, key) => {
             message += `${value.command} - ${value.description}\n`;
-            message += `Usage: ${value.usage}\n---------------\n`;
+            message += `Usage: ${BotMaster.instance.config.prefix}${value.usage}\n---------------\n`;
         });
         message += '```';
         msg.channel.send(message);
