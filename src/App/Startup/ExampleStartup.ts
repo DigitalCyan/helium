b57598183f95c50fdecdafa363c113d05b159895
DigitalCyan <marinoljubetic8@gmail.com>
2020-12-17 17:31:32 +0000

    Cleanup

--- src/App/Startup/ExampleStartup.ts
+++ src/App/Startup/ExampleStartup.ts
@@ -4,7 +4,7 @@ import Helium from '../../Core/Classes/Helium/Helium';
 export default () => {
     Helium.instance.client.on('message', (message: Message) => {
         if (message.author.id != Helium.instance.client.user.id) {
-            message.reply('k');
+            message.channel.send("This is a startup file that was designed to annoy you till you study it and remove it.");
         }
     });
 };
