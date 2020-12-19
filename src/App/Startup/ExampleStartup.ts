285dedaea5956bdef6bd45ac3328cad4a9516a65
DigitalCyan <marinoljubetic8@gmail.com>
2020-12-19 05:07:51 +0000

    Patched up ExampleStartup.ts to be less annoying

--- src/App/Startup/ExampleStartup.ts
+++ src/App/Startup/ExampleStartup.ts
@@ -2,9 +2,5 @@ import { Message } from 'discord.js';
 import Helium from '../../Core/Classes/Helium/Helium';
 
 export default () => {
-    Helium.instance.client.on('message', (message: Message) => {
-        if (message.author.id != Helium.instance.client.user.id) {
-            message.channel.send("This is a startup file that was designed to annoy you till you study it and remove it.");
-        }
-    });
+    console.log("This is a startup file that was designed to annoy you till you study it and remove it.");
 };
